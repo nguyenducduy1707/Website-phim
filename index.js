fbee9f5c1d2e54a666c60095f2dbeeeedc401809
nguyenducduy1707 <nguyenducduy1707@gmail.com>
2020-12-02 02:36:50 +0700

    responsive mobile

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function open_and_move2(){
     win3=window.open("slider7.html","","width=600,height=500")
     win3.moveTo(screen.width/2-300,screen.height/2-250)
 }
+//
+if($(window).width() <= 991) {
+    $(`#container-fantasy, #container-action, #container-romantic, #container-comedy, #container-horror`)
+    .removeClass("container")
+    .addClass("container-fluid")
+} else {
+    $(`#container-fantasy, #container-action, #container-romantic, #container-comedy, #container-horror`)
+    .removeClass("container-fluid")
+    .addClass("container")
+}
