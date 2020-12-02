4aee3de7617febb7f5011e2b242441c3199102f0
nguyenducduy1707 <nguyenducduy1707@gmail.com>
2020-12-02 14:33:00 +0700

    2/12/2020

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function open_and_move2(){
     win3=window.open("slider7.html","","width=600,height=500")
     win3.moveTo(screen.width/2-300,screen.height/2-250)
 }
-//
+// Responsive slide
 if($(window).width() <= 991) {
     $(`#container-fantasy, #container-action, #container-romantic, #container-comedy, #container-horror`)
     .removeClass("container")
@@ -18,3 +18,14 @@ if($(window).width() <= 991) {
     .removeClass("container-fluid")
     .addClass("container")
 }
+$(window).resize(function() {
+    if($(window).width() <= 991) {
+        $(`#container-fantasy, #container-action, #container-romantic, #container-comedy, #container-horror`)
+        .removeClass("container")
+        .addClass("container-fluid")
+    } else {
+        $(`#container-fantasy, #container-action, #container-romantic, #container-comedy, #container-horror`)
+        .removeClass("container-fluid")
+        .addClass("container")
+    }
+})
